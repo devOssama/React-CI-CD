6f76c2a44a20f21c6762aec226040581e233df7b
ossama <hichamiossama@gmail.com>
2021-02-10 14:15:19 +0100

    add circleci config

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,8 @@
-import { render } from '@testing-library/react';
-import Enzyme, { mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import Quotes from './components/Quotes';
 
-// Add your adapter version below
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-test('displays a quote', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const quote = document.querySelector('#text p');
-  expect(quote).toBeInTheDocument();
-  expect(quote).not.toBeEmptyDOMElement();
-});
-
-it('calls generateRandomQuote prop function when next button is clicked', () => {
-  const generateRandomQuoteFn = jest.fn();
-  const quote = mount(
-    <Quotes generateRandomQuote={generateRandomQuoteFn} quote={{}} />
-  );
-  const generateBtn = quote.find('#new-quote');
-
-  generateBtn.simulate('click');
-  expect(generateRandomQuoteFn).toHaveBeenCalledTimes(1);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
